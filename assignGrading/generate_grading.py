7a580be3909adda29dfd214cd71ef013ae05352d
KAYLIA1 <kaylia.mai@gmail.com>
2024-12-02 14:44:49 -0500

    last commit

--- assignGrading/generate_grading.py
+++ assignGrading/generate_grading.py
@@ -13,7 +13,11 @@ types = {
 
 allGraders = 21
 numGraders = allGraders
-numSubmissions = 473
+numSubmissions = 515
+
+addFailed = True
+failedSubmissions = 18
+failedFile = "FailedAutograder.txt"
 
 class item(object):
     def __init__(self, name, type, numPpl=None):
@@ -21,7 +25,28 @@ class item(object):
         self.type = type
         self.numPpl = numPpl
 
-def splitItem(result, curItem, ppl, submissions):
+def printMode(result, curItem, first, last, mode = 0): # 0=normal, 1=failed submissions by name
+    if(first>last):
+        return
+    if(mode == 1):
+        f = open(failedFile, "r")
+        names = f.readlines()
+        f.close()
+        curResult = curItem.name+":"
+        for i in range(first-1, last):
+            if(i!=first-1):
+                curResult += ", "
+            curResult += str(names[i].strip())
+        result.append(curResult)
+        f.close()
+    elif(mode == 0):
+        if(first!=last):
+            result.append(curItem.name+":"+str(first)+"-"+str(last))
+        else:
+            result.append(curItem.name+":"+str(first))
+        
+
+def splitItem(result, curItem, ppl, submissions, mode = 0):
     numTask = int(math.ceil(submissions/ppl))
     print(curItem.name + " | ppl:" + str(ppl)+" submissions/person:"+str(numTask))
     pplExtraWork = submissions%ppl # num ppl that will have 1 more task to do than others
@@ -31,12 +56,18 @@ def splitItem(result, curItem, ppl, submissions):
         last = first+numTask-1
         if(i>=pplExtraWork):
             last -= 1
-        print(" "+str(first)+" "+str(last))
-        result.append(curItem.name+":"+str(first)+"-"+str(last))
+        if(i == ppl-1): # almost there bug # TODO This is unfair to last person oop, but ceil so mb ok
+            last = submissions
+        # print(" "+str(first)+" "+str(last))
+        printMode(result, curItem, first, last, mode)
+        # if(first!=last):
+        #     result.append(curItem.name+":"+str(first)+"-"+str(last))
+        # else:
+        #     result.append(curItem.name+":"+str(first))
 
 allQs = [
-item("Q1.1", "format", 2), item("Q1.2", "format",2), item("Q1.3", "format",2), item("Q1.4", "format",2),
-item("Q3", "longans"), item("Q4.2", "longans")
+item("Q1", "format", 4),
+item("Q3.1", "format", 21)
 ]
 
 remainingQs = []
@@ -55,6 +86,7 @@ for curItem in allQs:
     totalGW += types[curItem.type]
     if(curItem.numPpl==None):
         gradeWeight += types[curItem.type]
+        print("skipping")
     else:
         # ans.append(curItem.name+":"+)
         splitItem(ans, curItem, curItem.numPpl, numSubmissions)
@@ -71,27 +103,35 @@ print()
 remainingGraders = numGraders
 # numGraders/gradeWeight is num people per Q
 for curItem in remainingQs:
-    numPpl = int(math.ceil(numGraders*types[curItem.type]/gradeWeight))
+    numPpl = int(math.ceil(numGraders*types[curItem.type]/gradeWeight)) # people assigned to this item
     remainingGraders -= numPpl
     if(remainingGraders < 0): # oops we don't have that many people
         numPpl += remainingGraders
     
+    print(str(numPpl)+" "+str(remainingGraders))
+    
     splitItem(ans, curItem, numPpl, numSubmissions)
-    # numTask = int(math.ceil(numSubmissions/numPpl))
-    # remainingSubmissions = numSubmissions
-    # print(curItem.name + " | " + str(numPpl)+" "+str(numTask))
-    # pplExtraWork = numSubmissions%numPpl # num ppl that will have 1 more task to do than others
-    # print("ppl w extra work: "+str(pplExtraWork))
-    # last = 0 # first submission starts at 1
-    # for i in range(numPpl):
-    #     first = last+1
-    #     last = first+numTask-1
-    #     if(i>=pplExtraWork):
-    #         last -= 1
-    #     print(" "+str(first)+" "+str(last))
-    #     ans.append(curItem.name+":"+str(first)+"-"+str(last))
+
+failedAns = []
+if(addFailed):
+    splitItem(failedAns, item("Failed", "longans"), allGraders, failedSubmissions, 1)
 
 print(ans)
+print(len(ans))
+print("------------------")
+
+print("[", end ="")
+for i in range(len(ans)):
+    print("\"", end ="")
+    print(ans[i], end ="")
+    if(addFailed and i<len(failedAns)):
+        print(" "+str(failedAns[i]), end="")
+    print("\"", end ="")
+    if(not (i==len(ans)-1)):
+        print(", ", end ="")
+print("]", end ="")
+print()
+
 print("Reminders:")
 print(" - Create to-do list and select it")
 print(" - Set due date")
